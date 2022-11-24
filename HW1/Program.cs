c0e214c21f558f9bfcc3ce2c30c31928a3ce44b9
forcesumetee <46419997+forcesumetee@users.noreply.github.com>
2022-11-24 18:45:36 +0700

    first commit

---
+++ HW1/Program.cs
@@ -0,0 +1,75 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        char vars;  
+        string start = "stay01";
+        int number = 0;    
+        string bools = "false";
+        CircularLinkedList<char> character = new CircularLinkedList<char>();
+        character.Add('J');
+        character.Add('G'); 
+        character.Add('O'); 
+        character.Add('R'); 
+        CircularLinkedList<char> Inputchar = new CircularLinkedList<char>();
+        do
+        {
+            vars = Inputcharacter();    
+            if (vars == character.Get(0) || vars == character.Get(1) 
+            || vars == character.Get(2) || vars == character.Get(3))
+               {
+                    bools = "true"; 
+                    Inputchar.Add(vars);                                                     
+                    if (Inputchar.Get(number) == 'R' && start == "stay01")  
+                    {
+                       Console.WriteLine("*****************");
+                       Console.WriteLine("Invalid pattern.");
+                       Inputchar.Set(0,Inputcharacter());                      
+                    }  
+                    else if (Inputchar.Get(number-1) == 'R' && Inputchar.Get(number-2) == Inputchar.Get(number))  
+                    {
+                       Console.WriteLine("*****************");
+                       Console.WriteLine("Invalid pattern.");
+                       Inputchar.Remove(number);
+                       number--;
+                    }  
+                    else if(Inputchar.Get(number) == 'G' && Inputchar.Get(number-1) == 'G' 
+                    && Inputchar.Get(number-2) == 'G' && Inputchar.Get(number-3) == 'G' && number > 2)
+                    {
+                       Console.WriteLine("*****************");
+                       Console.WriteLine("Invalid pattern.");
+                       Inputchar.Remove(number);
+                       number--;
+                    } 
+                    else if(Inputchar.Get(number) == 'G' && Inputchar.Get(number+1) == 'G' 
+                    && Inputchar.Get(number+2) == 'G' && Inputchar.Get(number+3) == 'G' && number > 2)
+                    {
+                       Console.WriteLine("*****************");
+                       Console.WriteLine("Invalid pattern.");
+                       Inputchar.Remove(number);
+                       number--;
+                    }                   
+                    number++;   
+                    start = "stay00";                                                                                              
+               }             
+            else 
+            {
+                 bools = "false"; 
+            }          
+                     
+        }
+        while(bools == "true");
+        Console.WriteLine("*****************");
+        for (int i = 0;i < Inputchar.GetLength();i++)
+        { 
+            Console.Write(Inputchar.Get(i));
+        }
+    }
+    static char Inputcharacter()
+    {
+        Console.WriteLine("*****************");
+        Console.Write("Input flower : ");
+        return char.Parse(Console.ReadLine());
+    }
+
+}
---
+++ HW2/Program.cs
@@ -0,0 +1,60 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        char vars; 
+        int i = 0;   
+        string bools = "true"; 
+        Queue<char> size = new Queue<char>();
+        size.Push('L'); 
+        size.Push('M'); 
+        size.Push('S'); 
+        Queue<char> number = new Queue<char>();
+        do
+        {
+            vars = Inputcharacter();    
+            if (vars == size.Get(0)) 
+            {                                      
+               number.Push('1');                                                  
+            } 
+            else if (vars == size.Get(1))
+            {
+                number.Push('2');
+            } 
+            else if (vars == size.Get(2))
+            {
+                number.Push('3');
+            }            
+            else 
+            {
+                 bools = "false"; 
+            }                             
+        }
+        while(bools == "true");  
+        for (i = 0; i < number.GetLength();i++)
+        {
+            if(number.Get(i) == '1')
+            {
+                number.Push('2');
+                number.Push('2');
+            }
+            else if(number.Get(i) == '2')
+            {
+                number.Push('3');
+                number.Push('3');
+                number.Push('3');
+            }
+        }
+        Console.WriteLine("*****************");
+        for (i = 0;i < number.GetLength();i++)
+        {
+            Console.Write(number.Get(i));
+        }    
+    }
+    static char Inputcharacter()
+    {
+        Console.WriteLine("*****************");
+        Console.Write("Input fruit : ");
+        return char.Parse(Console.ReadLine());
+    }
+}
